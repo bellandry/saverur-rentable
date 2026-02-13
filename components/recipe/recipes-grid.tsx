5679c6c224125351c69992a70e49e3f955bc6c30
bellandry <bellandry.work@gmail.com>
2026-02-13 17:20:08 +0100

    update(layout): recipe grids to fit with the newrecipe properties

--- components/recipe/recipes-grid.tsx
+++ components/recipe/recipes-grid.tsx
@@ -1,17 +1,23 @@
 "use client";
 
-import { FEATURED_RECIPES } from "@/constant";
+import { Recipe } from "@/types";
 import Link from "next/link";
 import { Button } from "../ui/button";
 import RecipeCard from "./recipe-card";
 
-const RecipeGrid = () => {
+const RecipeGrid = ({
+  recipes,
+  title,
+}: {
+  recipes: Recipe[];
+  title: string;
+}) => {
   return (
     <section className="py-12 bg-cream">
       <div className="container mx-auto px-6">
         <div className="flex items-center justify-between mb-12">
           <h2 className="text-3xl font-bold font-serif text-darkBrown">
-            Latest Recipes
+            {title}
           </h2>
           <Button
             asChild
@@ -22,7 +28,7 @@ const RecipeGrid = () => {
         </div>
 
         <div className="flex overflow-x-auto pb-6 -mx-6 px-6 gap-4 md:grid md:grid-cols-2 lg:grid-cols-4 md:overflow-visible md:pb-0 md:mx-0 md:px-0 snap-x snap-mandatory md:snap-none scrollbar-hide">
-          {FEATURED_RECIPES.map((recipe) => (
+          {recipes.map((recipe) => (
             <div
               key={recipe.id}
               className="min-w-[75vw] sm:min-w-[300px] md:min-w-0 snap-center bg-transparent rounded-2xl overflow-hidden transition-all duration-500 group cursor-pointer border border-beige/20 flex flex-col h-full"
