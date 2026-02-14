c1132d97ea8dabcc005560d901944c4ccfac50c3
bellandry <bellandry.work@gmail.com>
2026-02-14 11:36:12 +0100

    update(layout): create frontend app folder & move all landing to it

--- app/(frontend)/page.tsx
+++ app/(frontend)/page.tsx
@@ -1,9 +1,7 @@
 import About from "@/components/about-chef";
 import Categories from "@/components/categories";
 import Collections from "@/components/collection";
-import Footer from "@/components/footer";
 import Hero from "@/components/hero";
-import Navbar from "@/components/navbar";
 import Newsletter from "@/components/newsletter";
 import { PopularRecipes } from "@/components/recipe/popular-recipes";
 import RecipeGrid from "@/components/recipe/recipes-grid";
@@ -12,7 +10,6 @@ import { FEATURED_RECIPES, LATEST_RECIPES, POPULAR_RECIPES } from "@/constant";
 export default function Home() {
   return (
     <div className="min-h-screen bg-zinc-50 font-sans dark:bg-black">
-      <Navbar />
       <Hero />
       <Categories />
       <RecipeGrid recipes={LATEST_RECIPES} title="Latest Recipes" />
@@ -21,7 +18,6 @@ export default function Home() {
       <PopularRecipes popularRecipes={FEATURED_RECIPES} />
       <About />
       <Newsletter />
-      <Footer />
     </div>
   );
 }
