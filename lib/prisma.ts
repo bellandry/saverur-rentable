f24e440bed9dabcaea43b0387342a0d5aa9d2826
bellandry <bellandry.work@gmail.com>
2026-02-18 07:28:42 +0100

    update: remove console log on prisma helper

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,6 +1,6 @@
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
-console.log("DATABASE_URL:", process.env.DATABASE_URL);
+
 const connectionString = process.env.DATABASE_URL!;
 
 const adapter = new PrismaPg({ connectionString });
